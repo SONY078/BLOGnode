08252f1d58e4271d3c5980302d7983a86d9de02f
N Sony <71254514+SONY078@users.noreply.github.com>
2022-08-01 09:48:17 +0530

    Update Data.js

--- Routes/Data.js
+++ Routes/Data.js
@@ -2,7 +2,7 @@ const express = require ("express")
 const datacontroller = require("../Controllers/Content")
 
 const DataRouter = express.Router()//we are linking our data.js file with our /api main
-DataRouter.route("/details")//creating a route
+DataRouter.route("/content")//creating a route
 .get(datacontroller.ApiController)//calling the data.js in Controller folder
 
 module.exports = DataRouter
